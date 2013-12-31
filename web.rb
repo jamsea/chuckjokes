76c645c93081e8a22e0384f4296d59df159919a5
Jamie Hush <jhush91@gmail.com>
2013-12-31 14:50:58 -0500

    Added support for voice.

--- web.rb
+++ web.rb
@@ -3,10 +3,18 @@ require 'httparty'
 require 'twilio-ruby'
 require 'sinatra'
 
-get '/' do
+get '/text' do
 	response = HTTParty.get('http://api.icndb.com/jokes/random')
 	twiml = Twilio::TwiML::Response.new do |r|
 		r.Message  response['value']['joke']
+	end
+	twiml.text
+end
+
+get '/voice' do
+	response = HTTParty.get('http://api.icndb.com/jokes/random')
+	twiml = Twilio::TwiML::Response.new do |r|
+		r.Say  response['value']['joke']
 	end
 	twiml.text
 end
